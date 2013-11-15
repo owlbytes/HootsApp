83f58e1bf295269b9648b2fbe4d980978c1deae4
owlbytes <neshah07@gmail.com>
2013-11-15 14:20:43 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HootsApp::Application.config.secret_token = '946ff0f4621aa480c4e558085dc52a4996e19212888157ced198a61feaa4604bd8766d5f9302db34ad615f1ae413dad2d97892c4e3a7e9cd699e73317a4cae11'
