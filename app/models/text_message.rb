4a57be9f2a098a56379accded398d56e854e1cb5
jamiewallace <wallacejamiec@gmail.com>
2013-11-19 15:42:39 +0000

    prep for twilio

---
+++ app/models/text_message.rb
@@ -0,0 +1,18 @@
+# class TextMessage
+#   def initialize content, to_number
+#     @content, @to_number = content, to_number
+#     self
+#   end
+
+#   def client
+#     @client ||= Twilio::REST::Client.new(ENV["TWILIO_SID"], ENV["TWILIO_AUTH"])
+#   end
+
+#   def send
+#     client.account.sms.messages.create(
+#       body: @content,
+#       to: @to_number,
+#       from: ENV["TWILIO_NUM"]
+#     )
+#   end
+# end
