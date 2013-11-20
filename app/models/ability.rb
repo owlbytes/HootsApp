8464737ca250c760f8ef0bb8af4b430cd366b1d6
AdamBuchan <adam.buchan@gmail.com>
2013-11-20 21:31:56 +0000

    not sure what I've added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,32 +3,31 @@ class Ability
 
   def initialize(user)
     user ||= User.new
+    
+    can :read, :all
+    can :create, User
+    
     if user.role == 'admin'
       can :manage, :all
+    
     elsif user.role == 'user'
-      can :manage, :all
-      # can :read, :all
-      # can :create, Post
-      # if post.user == current_user
-      #   can :update, Post
-      #   can :destroy, Post
-      # end
-      # can :update, User do |u|
-      #   u == user
-      # end
-      # can :destroy, User do |u|
-      #   u == user
-      # end
-      # can :create, Post
-      # can :update, Post do |post|
-      #   post.user == user
-      # end
+      can :create, Post
+      can :update, Post do |post|
+        post.user == user
+      end
       # can :destroy, Post do |post|
       #   post.user == user
       # end
-    else
-      can :read, :all
-      can :create, User
+      
+      # can :manage, Comment
+
+      # can :update, User do |u|
+      #   u.id == user.id
+      # end
+      # can :destroy, User do |u|
+      #   u.id == user.id
+      # end
+
     end
   end
 end
