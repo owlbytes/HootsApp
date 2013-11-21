31419587e2afb8a2a7dcdc9c163dd53e0a98263c
jamiewallace <wallacejamiec@gmail.com>
2013-11-21 21:40:23 +0000

    more images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def randomized_post_image()
-    images = ["London1.jpeg", "dark_sunset.jpg", "purple_forest.png", "brickwall_1", "fireflies-in-the-forest-309979.jpg", "fireworks.jpg", "venus.jpg", "aurora.jpg", "planet_ocean.jpg", "Green-Fields.jpg", "sunset.jpg"]  
+    images = ["London1.jpeg", "dark_sunset.jpg", "purple_forest.png", "fireflies-in-the-forest-309979.jpg", "fireworks.jpg", "venus.jpg", "aurora.jpg", "planet_ocean.jpg", "Green-Fields.jpg", "sunset.jpg", "swiss.jpg", "rainbow.jpg", "darkcity.jpg", "deep_blue_sea_by_mudukrull.jpg", "giraffe-sunset-wallpaper.jpg", "orangeleaf.jpg", "seasunset.jpg", "seasunset2.jpg", "seasunset3.jpg"]  
     images[rand(images.size)]
   end
 
