b5852df14b5097a320e530a40659a7a3121e31b1
AdamBuchan <adam.buchan@gmail.com>
2013-11-21 09:08:03 +0000

    testing bug fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,17 +4,23 @@ class Ability
   def initialize(user)
     user ||= User.new
     
-    can :read, :all
-    can :create, User
     
     if user.role == 'admin'
       can :manage, :all
     
     elsif user.role == 'user'
-      can :create, Post
-      can :update, Post do |post|
+
+      # can :update, Post, :user_id => user.id
+      can :read, :all
+      can [:create, :update], Post do |post|
+        binding.pry
         post.user == user
       end
+      # can :create, Post
+      # can :create, Post
+      # can :update, Post do |post|
+      #   post.user == user
+      # end
       # can :destroy, Post do |post|
       #   post.user == user
       # end
@@ -27,6 +33,10 @@ class Ability
       # can :destroy, User do |u|
       #   u.id == user.id
       # end
+
+    else
+      can :read, :all
+      can :create, User
 
     end
   end
